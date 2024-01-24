b191610fc469477d5fa468992b0eddcf08cb7ff7
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-01-23 19:36:40 -0600

    Refactor Navbar component styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -25,10 +25,10 @@ export default function Navbar() {
   }
 
   return (
-    <div>
-      <div className='flex flex-row justify-between h-16 p-10 items-center w-screen'>
+    <div className={isNavOpen? 'shadow-md' : null}>
+      <div className={`flex flex-row justify-between h-16 px-3 md:p-10 items-center w-screen ${isNavOpen ? null: 'shadow-md'}`}>
         <Link onClick={() => setIsNavOpen(false)} href='/'><p className={`${pathName === '/' ? 'text-gray-500 underline decoration-4 underline-offset-8 dark:text-yellow-200' : null} text text-gray-500 dark:text-yellow-50 hover:cursor-pointer font-bold text-2xl`}>#prajwl-dh</p></Link>
-        <div className='flex flex-row gap-10 text-gray-500 dark:text-yellow-50 text-lg hover:cursor-pointer font-bold items-center'>
+        <div className='flex flex-row gap-5 md:gap-10 text-gray-500 dark:text-yellow-50 text-lg hover:cursor-pointer font-bold items-center'>
           <Link className={`hidden md:inline hover:text-gray-800 dark:hover:text-yellow-200 ${pathName === '/about' ? 'text-gray-800 underline decoration-4 underline-offset-4 dark:text-yellow-200' : null}`} href='/about'>About</Link>
           <Link className={`hidden md:inline hover:text-gray-800 dark:hover:text-yellow-200 ${pathName === '/projects' ? 'text-gray-800 underline decoration-4 underline-offset-4 dark:text-yellow-200' : null}`} href='/projects'>Projects</Link>
           <Link className={`hidden md:inline hover:text-gray-800 dark:hover:text-yellow-200 ${pathName === '/contact' ? 'text-gray-800 underline decoration-4 underline-offset-4 dark:text-yellow-200' : null}`} href='/contact'>Contact</Link>
@@ -36,17 +36,17 @@ export default function Navbar() {
             theme === "dark" ? <svg onClick={() => {setTheme("light")}} className="text text-2xl transition duration-300 ease-in-out hover:scale-125" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5s5-2.24 5-5s-2.24-5-5-5M2 13h2c.55 0 1-.45 1-1s-.45-1-1-1H2c-.55 0-1 .45-1 1s.45 1 1 1m18 0h2c.55 0 1-.45 1-1s-.45-1-1-1h-2c-.55 0-1 .45-1 1s.45 1 1 1M11 2v2c0 .55.45 1 1 1s1-.45 1-1V2c0-.55-.45-1-1-1s-1 .45-1 1m0 18v2c0 .55.45 1 1 1s1-.45 1-1v-2c0-.55-.45-1-1-1s-1 .45-1 1M5.99 4.58a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0s.39-1.03 0-1.41zm12.37 12.37a.996.996 0 0 0-1.41 0a.996.996 0 0 0 0 1.41l1.06 1.06c.39.39 1.03.39 1.41 0a.996.996 0 0 0 0-1.41zm1.06-10.96a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0zM7.05 18.36a.996.996 0 0 0 0-1.41a.996.996 0 0 0-1.41 0l-1.06 1.06c-.39.39-.39 1.03 0 1.41s1.03.39 1.41 0z"></path></svg> : 
             <svg onClick={() => {setTheme("dark")}} className="text text-2xl transition duration-300 ease-in-out hover:scale-125" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M11.01 3.05C6.51 3.54 3 7.36 3 12a9 9 0 0 0 9 9c4.63 0 8.45-3.5 8.95-8c.09-.79-.78-1.42-1.54-.95A5.403 5.403 0 0 1 11.1 7.5c0-1.06.31-2.06.84-2.89c.45-.67-.04-1.63-.93-1.56"></path></svg>
           }
-          <svg onClick={navigationToggle} className={`text text-2xl md:hidden`} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"/></svg>
+          <motion.div animate={isNavOpen? {rotate: 90} : {rotate: 0}} transition={{type: "spring", duration: "0.8"}}>
+            <svg onClick={navigationToggle} className={`text text-2xl md:hidden`} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"/></svg>
+          </motion.div>
         </div>
       </div>
       {
-        isNavOpen?
-        <motion.div initial={{opacity: 0, scale:0}} animate={isNavOpen? { opacity: 1, scale : 1 } : {opacity: 0, scale: 0}} transition={{ delay: 0.08}} className='md:hidden flex flex-col mx-10 shadow-md bg-neutral-800 dark:bg-yellow-50 min-h-24 text-yellow-50 dark:text-gray-500 text-xl hover:cursor-pointer font-bold items-center'>
+        <motion.div initial={{opacity: 0, scale:0}} animate={isNavOpen? { opacity: 1, scale : 1 } : {opacity: 0, scale: 0}} transition={{ delay: 0.01, type: "spring"}} className='md:hidden flex flex-col mx-6 mb-2 shadow-md bg-neutral-800 dark:bg-yellow-50 min-h-20 text-yellow-50 dark:text-gray-500 text-xl hover:cursor-pointer font-bold items-center'>
             <Link onClick={navigationToggle} className={`md:hidden mb-2 mt-5 hover:text-yellow-200 dark:hover:text-gray-800 ${pathName === '/about' ? 'text-yellow-200 underline decoration-4 underline-offset-4 dark:text-neutral-800' : null}`} href='/about'>About</Link>
             <Link onClick={navigationToggle} className={`md:hidden mb-2 hover:text-yellow-200 dark:hover:text-gray-800 ${pathName === '/projects' ? 'text-yellow-200 underline decoration-4 underline-offset-4 dark:text-neutral-800' : null}`} href='/projects'>Projects</Link>
             <Link onClick={navigationToggle} className={`md:hidden mb-5 hover:text-yellow-200 dark:hover:text-gray-800 ${pathName === '/contact' ? 'text-yellow-200 underline decoration-4 underline-offset-4 dark:text-neutral-800' : null}`} href='/contact'>Contact</Link>
-        </motion.div>:
-        null
+        </motion.div>
       }
     </div>
     
