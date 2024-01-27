ccb610ed3a4ca68fc610d80ace90ba2144f3de04
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-01-27 15:00:26 -0600

    Started projects page, also create a data.js file that contains information about my projects

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,12 +1,25 @@
 'use client'
 import React from 'react'
 import ScrollToTop from 'react-scroll-to-top'
+import githubProjects from '@/data/data'
 
 export default function Projects() {
   return(
     <>
-      <div className='w-full md:w-9/12 md:max-w-screen-xl self-center'>
-        <p className='text text-gray-600 dark:text-yellow-50 text-2xl pt-10 px-10'>Projects Page In Development</p>
+      <div className='w-full md:w-9/12 md:max-w-screen-xl self-center flex flex-col p-5 md:mt-5 md:p-0 mb-10'>
+        <p className='text text-2xl md:text-3xl font-bold text-gray-500 dark:text-yellow-50 mb-5'>Explore my personal projects</p>
+        <div className='w-full min-h-screen'>
+          <div className='flex flex-row md:gap-5 gap-3 flex-wrap'>
+            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Web</button>
+            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Games</button>
+            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Robotics</button>
+            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>AI</button>
+            <button className='text underline text-gray-500'>Clear Filters</button>
+          </div>
+          <div className='mt-5 w-full border border-red-500 min-h-screen p-5'>
+            {projectsElement}
+          </div>
+        </div>
       </div>
       <ScrollToTop smooth top="150" width="40" height="30" style={{marginBottom: "40px", backgroundColor: "#fefce8", outline: "4px white solid", borderRadius: "50%", cursor: "pointer"}}/>
     </>
---
+++ src/data/data.js
@@ -0,0 +1,54 @@
+const githubProjects = [
+  {
+    name: 'Test',
+    type: 'web',
+    link: 'www.google.com',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+  {
+    name: '',
+    type: '',
+    link: '',
+  },
+];
+
+export default githubProjects;
