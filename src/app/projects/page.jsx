9d86505c896430202727bba9ebc6045752db616a
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-01-30 16:46:30 -0600

    Styled projects page, also added onclick to github

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,42 +3,42 @@ import React from 'react'
 import ScrollToTop from 'react-scroll-to-top'
 import githubProjects from '@/data/data'
 import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/navigation'
 
 export default function Projects() {
-  const shuffleArray = githubProjects.sort((a, b) => 0.5 - Math.random())
+  const router = useRouter()
 
+  const shuffleArray = githubProjects.sort((a, b) => 0.5 - Math.random())
   const allProjects = shuffleArray.map((project) => {
     let gridStyle = ''
     if(project.type === 'web'){
-      gridStyle = 'bg-blue-400 dark:bg-cyan-600'
+      gridStyle = 'bg-cyan-500 dark:bg-cyan-600'
     }else if(project.type === 'game'){
-      gridStyle = 'bg-emerald-400 dark:bg-emerald-400'
-    }else if(project.type === 'robotics'){
-      gridStyle = 'bg-yellow-300 dark:bg-yellow-400'
+      gridStyle = 'bg-green-500 dark:bg-green-600'
     }else{
-      gridStyle = 'bg-rose-400 dark:bg-rose-700'
+      gridStyle = 'bg-rose-500 dark:bg-rose-700'
     }
-
     return(
-      <div className={`flex border-gray-700 p-5 ${gridStyle} dark:border-yellow-50 rounded-2xl shadow-xl h-60 items-center justify-center text-center`} key={uuidv4()}>
-        <div className='text font-bold text-3xl text-gray-500 dark:text-yellow-50'><q>{project.name}</q></div>
-      </div>
+      <a href={project.link} target="_blank" key={uuidv4()}>
+        <div className={`flex p-5 ${gridStyle} dark:border-yellow-50 border-2 border-gray-500 rounded-2xl shadow-xl h-60 items-center justify-center text-center`}>
+          <div className='text font-bold text-3xl text-yellow-50 dark:text-yellow-50'><q>{project.name}</q></div>
+        </div>
+      </a>
     )
   })
 
   return(
     <>
-      <div className='w-full md:w-9/12 md:max-w-screen-xl self-center flex flex-col p-5 md:mt-5 md:p-0 mb-10'>
+      <div className='w-full md:w-9/12 md:max-w-screen-xl self-center flex flex-col p-5 md:mt-10 md:p-0 mb-10'>
         <p className='text text-2xl md:text-3xl font-bold text-gray-500 dark:text-yellow-50 mb-5'>Explore my personal projects</p>
         <div className='w-full min-h-screen'>
-          <div className='flex flex-row md:gap-5 gap-3 flex-wrap'>
-            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Web</button>
-            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Games</button>
-            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>Robotics</button>
-            <button className='border-2 border-gray-400 text-gray-500 h-8 min-w-20 rounded-md'>AI</button>
-            <button className='text underline text-gray-500'>Clear Filters</button>
+          <div className='flex flex-row md:gap-5 gap-3 flex-wrap md:mt-2'>
+            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Web</button>
+            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Games</button>
+            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Robotics</button>
+            <button className='text underline text-gray-500 dark:text-yellow-50'>Clear Filters</button>
           </div>
-          <div className='mt-5 md:mt-10 w-full p-5 md:p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+          <div className='mt-5 md:mt-10 w-full p-5 md:p-0 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10'>
             {allProjects}
           </div>
         </div>
--- src/data/data.js
+++ src/data/data.js
@@ -7,25 +7,18 @@ const githubProjects = [
       'My codebase for this current client side rendered website using Next.js, React.js, TailwindCSS, and Framer Motion',
   },
   {
+    name: 'Tetris Game',
+    type: 'game',
+    link: 'https://github.com/prajwl-dh/tetrisJavaGUI',
+    description:
+      'A fully working tetris game written in java that features music, highscores, saving a game, and changing game themes in real time',
+  },
+  {
     name: 'A Blog Website',
     type: 'web',
     link: 'https://github.com/prajwl-dh/next-js-project',
     description:
       'Blog website with blogs, login/signup feature, Github authientication, admin-section powered by React.js, Next.js, CSS, MongoDB and NextAuth',
-  },
-  {
-    name: 'Vanlife Van Rental',
-    type: 'web',
-    link: 'https://github.com/prajwl-dh/van-rental-app',
-    description:
-      'A van rental service using React.js and react-router featuring protected routes and lazy loading. This projects utilizes features such as Actions, Loaders, etc from react-router',
-  },
-  {
-    name: 'Todo List App',
-    type: 'web',
-    link: 'https://github.com/prajwl-dh/to-do-list',
-    description:
-      'A todo list app using React.js, Redux and CSS that features adding, marking as completed, and removing todo. It also features saving todo to local storage',
   },
   {
     name: 'Tenzies game',
@@ -35,11 +28,25 @@ const githubProjects = [
       'A simple and fun SPA game created using React.js and CSS that utilizes useState and useEffects hooks',
   },
   {
+    name: 'Vanlife Van Rental',
+    type: 'web',
+    link: 'https://github.com/prajwl-dh/van-rental-app',
+    description:
+      'A van rental service using React.js and react-router featuring protected routes and lazy loading. This projects utilizes features such as Actions, Loaders, etc from react-router',
+  },
+  {
     name: 'Meme Generator',
     type: 'game',
     link: 'https://github.com/prajwl-dh/random-meme-generator',
     description:
       'A random meme generator app that fetches random images from the memes database and lets the user add their own captions',
+  },
+  {
+    name: 'Todo List App',
+    type: 'web',
+    link: 'https://github.com/prajwl-dh/to-do-list',
+    description:
+      'A todo list app using React.js, Redux and CSS that features adding, marking as completed, and removing todo. It also features saving todo to local storage',
   },
   {
     name: 'Tic Tac Toe game',
@@ -50,7 +57,7 @@ const githubProjects = [
   },
   {
     name: 'Object Shape Detection',
-    type: 'ai',
+    type: 'robotics',
     link: 'https://github.com/prajwl-dh/objectDetectionOpenCV',
     description:
       'An object detection program written in Python using OpenCV that can detect multi-colored squared foam cubes and draw a border around them',
@@ -75,13 +82,6 @@ const githubProjects = [
     link: 'https://github.com/prajwl-dh/gfcStore',
     description:
       'A complete shopping site with frontend and backend design that also features admin section for adding, removing and updating products in real time',
-  },
-  {
-    name: 'Tetris Game',
-    type: 'game',
-    link: 'https://github.com/prajwl-dh/tetrisJavaGUI',
-    description:
-      'A fully working tetris game written in java that features music, highscores, saving a game, and changing game themes in real time',
   },
 ];
 
