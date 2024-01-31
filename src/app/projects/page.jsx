ca3f36e161c4063ef8fd21ae90f51da82f48343c
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-01-30 18:27:47 -0600

    Enabled filters in project page using search params

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,15 +1,25 @@
 'use client'
-import React from 'react'
+import React, { useCallback } from 'react'
 import ScrollToTop from 'react-scroll-to-top'
 import githubProjects from '@/data/data'
 import { v4 as uuidv4 } from 'uuid';
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { motion } from 'framer-motion'
+import { useSearchParams } from 'next/navigation';
 
 export default function Projects() {
   const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
-  const shuffleArray = githubProjects.sort((a, b) => 0.5 - Math.random())
+  const createQueryString = useCallback((type, value) => {
+    const params = new URLSearchParams(searchParams)
+    value !== 'clear' ? params.set(type, value) : params.delete(type)
+    return params.toString()
+  }, [searchParams])
+
+  const filteredArray = searchParams.get('type') == null? githubProjects: githubProjects.filter((project) => project.type === searchParams.get('type'))
+  const shuffleArray = filteredArray.sort((a, b) => 0.5 - Math.random())
   const allProjects = shuffleArray.map((project) => {
     return(
       <motion.a initial={{ opacity: 0, y:10 }} whileInView={{ opacity: 1, y:0 }} transition={{duration: 2, type:"spring"}} href={project.link} target="_blank" key={uuidv4()}>
@@ -23,14 +33,14 @@ export default function Projects() {
   return(
     <>
       <div className='w-full md:w-9/12 md:max-w-screen-xl self-center flex flex-col p-5 md:mt-10 md:p-0 mb-10'>
-        <p className='text text-2xl md:text-3xl font-bold text-gray-500 dark:text-yellow-50 mb-5'>Explore my personal projects</p>
+        <motion.p initial={{ opacity: 0, y:0 }} whileInView={{ opacity: 1, y:0 }} transition={{duration: 2, type:"spring"}} className='text text-2xl md:text-3xl font-bold text-gray-500 dark:text-yellow-50 mb-5'>Explore my personal projects</motion.p>
         <div className='w-full min-h-screen'>
-          <div className='flex flex-row md:gap-5 gap-3 flex-wrap md:mt-2'>
-            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Web</button>
-            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Games</button>
-            <button className='border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-md'>Robotics</button>
-            <button className='text underline text-gray-500 dark:text-yellow-50'>Clear Filters</button>
-          </div>
+          <motion.div initial={{ opacity: 0, y:0 }} whileInView={{ opacity: 1, y:0 }} transition={{duration: 2, type:"spring"}} className='flex flex-row md:gap-5 gap-3 flex-wrap md:mt-2 text font-bold'>
+            <button onClick={() => router.push(pathname + '?' + createQueryString('type', 'web'))} className={`${searchParams.get('type') === 'web'? 'bg-gray-700 text-yellow-50 border-0 dark:bg-yellow-50 dark:text-neutral-700' : null} border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-lg`}>Web</button>
+            <button onClick={() => router.push(pathname + '?' + createQueryString('type', 'game'))} className={`${searchParams.get('type') === 'game'? 'bg-gray-700 text-yellow-50 border-0 dark:bg-yellow-50 dark:text-neutral-700' : null} border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-lg`}>Games</button>
+            <button onClick={() => router.push(pathname + '?' + createQueryString('type', 'robotics'))} className={`${searchParams.get('type') === 'robotics'? 'bg-gray-700 text-yellow-50 border-0 dark:bg-yellow-50 dark:text-neutral-700' : null} border-2 border-gray-500 text-gray-500 dark:border-yellow-50 dark:text-yellow-50 h-8 min-w-24 rounded-lg`}>Robotics</button>
+            <button onClick={() => router.push(pathname + '?' + createQueryString('type', 'clear'))} className='text underline text-gray-500 dark:text-yellow-50 font-normal'>Clear Filter</button>
+          </motion.div>
           <div className='mt-0 md:mt-10 w-full p-5 md:p-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-5 text-gray-500 dark:text-yellow-50'>
             {allProjects}
           </div>
