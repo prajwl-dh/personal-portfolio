914924b341777712ccdc88c29c06eab437c8edec
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-02-15 22:36:14 -0600

    Add @vercel/analytics package and import Analytics component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer/Footer';
 import Navbar from '@/components/navbar/Navbar';
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
           <Footer />
         </Providers>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
