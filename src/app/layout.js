9a616de9ce61aa97609e64ce7266c3c739015b65
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-04-01 19:05:01 -0500

    Update metadata in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title:
-    'Prajwal Dhungana - Frontend Engineer | Open Source Contributor | MERN Specialist | Full-Stack Skills with Node.js and Next.js',
+    'Prajwal Dhungana - Frontend Engineer | MERN Specialist | Full-Stack Skills with Node.js and Next.js | Open Source Contributor ',
   description:
     "Meet Prajwal Dhungana, a dedicated Frontend Engineer and Open Source Contributor specializing in the MERN stack, Next.js, and Tailwind CSS. With full-stack skills including Node.js, Prajwal is committed to continuous learning, ensuring his web development expertise remains cutting-edge and versatile. Explore Prajwal's portfolio for innovative web solutions blending modern design with functional excellence.",
 };
